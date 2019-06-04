0d92b70aa1581450c9eca8310776ca032df5a24d
DownedCrane <vovacrane@gmail.com>
2019-06-04 15:25:27 +0300

    Скрипты и деревня
    Добавил скрипты, поправил высоту объектов, убрал временно врагов чтобы лучше их настроить

---
+++ Assets/Scripts/ZoneTrigger.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZoneTrigger : MonoBehaviour
+{
+    // Объект для активации
+
+    [Header("Таг игрока, обычно Player")]
+    public string tag;
+
+    [Header("Если отмечено, то активируем объект")]
+    public bool state;
+
+    [Header("Объект для активации (со скриптами)")]
+    public GameObject myObject;
+    // Start is called before the first frame update
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag(tag))
+        {
+            myObject.gameObject.SetActive(state);
+            
+        }
+
+
+    }
+}
