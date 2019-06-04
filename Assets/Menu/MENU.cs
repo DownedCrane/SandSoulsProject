a88350c19ec779c20f86827c31213e3af8ba6e9d
DownedCrane <vovacrane@gmail.com>
2019-06-04 13:57:54 +0300

    Обновление скриптов
    Добавил сферу-триггер для активации скриптов при входе в деревню
    Все скрипты теперь в папке /scripts/
    Добавил сцену Тимы, пока не настраивал

---
+++ Assets/Menu/MENU.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MENU : MonoBehaviour
+{
+
+    public GameObject buttonsMenu;
+    public GameObject buttonsExit;
+    public GameObject buttonsSettings;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void ShowExitButtons()
+    {
+        buttonsMenu.SetActive(false);
+        buttonsExit.SetActive(true);
+        buttonsSettings.SetActive(false);
+    }
+    public void BackInMenu()
+    {
+        buttonsMenu.SetActive(true);
+        buttonsExit.SetActive(false);
+        buttonsSettings.SetActive(false);
+    }
+    public void ShowSettings()
+    {
+        buttonsMenu.SetActive(false);
+        buttonsExit.SetActive(false);
+        buttonsSettings.SetActive(true);
+    }
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+    public void NewGameLoadScenceStart()
+    {
+        Application.LoadLevel("Start");
+    }
+
+
+}
+
---
+++ Assets/Menu/SerGraphScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SerGraphScript : MonoBehaviour {
+
+    public Toggle LowGr;
+    public Toggle MediumGr;
+    public Toggle HighGr;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Graphics()
+    {
+        if (LowGr.isOn == true)
+        {
+            QualitySettings.currentLevel = QualityLevel.Fast;
+        }
+        if (MediumGr.isOn == true)
+        {
+            QualitySettings.currentLevel = QualityLevel.Good;
+        }
+        if (HighGr.isOn == true)
+        {
+            QualitySettings.currentLevel = QualityLevel.Fantastic;
+        }
+    }
+
+}
