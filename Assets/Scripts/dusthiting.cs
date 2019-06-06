58985adf30b3f0895dc9a99bd3fbc74a9cf23593
timnevidim <timonrussian@gmail.com>
2019-06-06 03:21:51 +0300

    Ещё фигня

---
+++ Assets/Scripts/dusthiting.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dusthiting : MonoBehaviour
+{
+    public Collision collision;
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Debug.Log("Go away!");
+        }
+
+    }
+
+    void OnTriggerEnter(Collider dust)
+    {
+        Debug.Log("Enter");
+    }
+    void OnTriggerStay(Collider dust)
+    {
+        Debug.Log("stay");
+    }
+   void OnTriggerExit(Collider dust)
+    {
+        Debug.Log("exit");
+    }
+}
