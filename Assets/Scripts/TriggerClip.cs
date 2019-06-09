b4968018f165f3b5085e854971f8df7df30f658b
timnevidim <timonrussian@gmail.com>
2019-06-09 17:43:32 +0300

    Сделал демо кат-сцену в начале
    Перелет камеры и включение/выключение текста

---
+++ Assets/Scripts/TriggerClip.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerClip : MonoBehaviour {
+	public GameObject Camera;
+	public float TimeEnd;
+	public GameObject Player;
+    public GameObject DialogeCanvas;
+
+    private float TimeStart;
+	private bool OnZone;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (OnZone == true) {
+			TimeStart += 1 * Time.deltaTime;
+			if (TimeStart >= TimeEnd) {
+				Player.SetActive (true);
+                DialogeCanvas.SetActive(false);
+
+                Camera.SetActive (false);
+				Destroy (gameObject);
+			}
+		}
+	}
+	void OnTriggerEnter(Collider col)
+	{
+		if (col.tag == "Player") {
+
+            DialogeCanvas.SetActive(true);
+            Camera.SetActive (true);
+			Player.SetActive (false);
+			OnZone = true;
+		}
+	}
+}
